9024ece41a9b71c9158a136299b38220b8c0cd3e
Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
2019-04-04 23:12:32 +0530

    Added binary tree traversals

--- binarytree/binarytree.go
+++ binarytree/binarytree.go
@@ -1,5 +1,11 @@
 package binarytree
 
+import (
+	"fmt"
+	"github.com/gowthamgts/go-algorithms/queue"
+	"github.com/gowthamgts/go-algorithms/stack"
+)
+
 type LessThanComparable func(a interface{}, b interface{}) bool
 
 type BinaryTree struct {
@@ -49,4 +55,162 @@ func (tree *BinaryTree) Insert(item interface{}) {
 			tree.right.Insert(item)
 		}
 	}
+}
+
+func (tree *BinaryTree) PrintLRN() {
+
+	if tree.left != nil {
+		tree.left.PrintLRN()
+	}
+
+	if tree.right != nil {
+		tree.right.PrintLRN()
+	}
+
+	if tree.node != nil {
+		fmt.Print(string(tree.node.(int32)))
+		return
+	}
+}
+
+func (tree *BinaryTree) PrintNLR() {
+	if tree.node != nil {
+		fmt.Print(string(tree.node.(int32)))
+	}
+
+	if tree.left != nil {
+		tree.left.PrintNLR()
+	}
+
+	if tree.right != nil {
+		tree.right.PrintNLR()
+	}
+
+	return
+}
+
+func (tree *BinaryTree) PrintLNR() {
+	if tree.left != nil {
+		tree.left.PrintLNR()
+	}
+
+	if tree.node != nil {
+		fmt.Print(string(tree.node.(int32)))
+	}
+
+	if tree.right != nil {
+		tree.right.PrintLNR()
+	}
+
+	return
+}
+
+func (tree *BinaryTree) PrintRNL() {
+
+	if tree.right != nil {
+		tree.right.PrintRNL()
+	}
+
+	if tree.node != nil {
+		fmt.Print(string(tree.node.(int32)))
+	}
+
+	if tree.left != nil {
+		tree.left.PrintRNL()
+	}
+
+	return
+}
+
+func (tree *BinaryTree) IterativePreOrder() {
+
+	s := stack.New()
+	s.Push(tree)
+
+	for !s.IsEmpty() {
+
+		tNode := s.Pop().(*BinaryTree)
+
+		if tNode.node != nil {
+			fmt.Print(string(tNode.node.(int32)))
+		}
+
+		if tNode.right != nil {
+			s.Push(tNode.right)
+		}
+
+		if tNode.left != nil {
+			s.Push(tNode.left)
+		}
+	}
+}
+
+func (tree *BinaryTree) IterativeInOrder() {
+	s := stack.New()
+
+	for !s.IsEmpty() || tree != nil {
+		if tree != nil {
+			s.Push(tree)
+			tree = tree.left
+		} else {
+			// pop and visit
+			tree = s.Pop().(*BinaryTree)
+			if tree.node != nil {
+				fmt.Print(string(tree.node.(int32)))
+			}
+			tree = tree.right
+		}
+	}
+}
+
+func (tree *BinaryTree) IterativeBFS() {
+	q := queue.New()
+
+	q.Enqueue(tree)
+
+	for !q.IsEmpty() {
+		qNode := q.Dequeue().(*BinaryTree)
+		if qNode.node != nil {
+			fmt.Print(string(qNode.node.(int32)))
+		}
+
+		if qNode.left != nil {
+			q.Enqueue(qNode.left)
+		}
+
+		if qNode.right != nil {
+			q.Enqueue(qNode.right)
+		}
+	}
+}
+
+func (tree *BinaryTree) MorrisTraversal() {
+
+	current := tree
+
+	for current != nil {
+		if current.left == nil {
+			if current.node != nil {
+				fmt.Print(string(current.node.(int32)))
+			}
+			current = current.right
+		} else {
+			leftRoot := current.left
+			for leftRoot.right != nil && leftRoot.right != current {
+				leftRoot = leftRoot.right
+			}
+
+			if leftRoot.right == nil {
+				leftRoot.right = current
+				current = current.left
+			} else {
+				leftRoot.right = nil
+				if current.node != nil {
+					fmt.Print(string(current.node.(int32)))
+				}
+				current = current.right
+			}
+		}
+	}
+
 }
--- binarytree/binarytree_test.go
+++ binarytree/binarytree_test.go
@@ -1,9 +1,12 @@
 package binarytree
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func LessThanComp(x interface{}, y interface{}) bool {
-	return x.(int) < y.(int)
+	return x.(int32) < y.(int32)
 }
 
 func TestBinaryTree_Insert(t *testing.T) {
@@ -34,4 +37,50 @@ func TestBinaryTree_Insert(t *testing.T) {
 	if rightTree == nil {
 		t.Error("Tree not found")
 	}
+}
+
+
+func TestBinaryTree_PrintValues(t *testing.T) {
+	tree := New(LessThanComp)
+
+	tree.Insert('F')
+	tree.Insert('B')
+	tree.Insert('G')
+	tree.Insert('A')
+	tree.Insert('D')
+	tree.Insert('I')
+	tree.Insert('C')
+	tree.Insert('E')
+	tree.Insert('H')
+	fmt.Print("PostOrder: LRN - ")
+	tree.PrintLRN()
+	fmt.Println()
+
+	fmt.Print("PreOrder: NLR - ")
+	tree.PrintNLR()
+	fmt.Println()
+
+	fmt.Print("InOrder: LNR - ")
+	tree.PrintLNR()
+	fmt.Println()
+
+	fmt.Print("OutOrder: RNL - ")
+	tree.PrintRNL()
+	fmt.Println()
+
+	fmt.Print("Iterative Preorder (stack): NLR - ")
+	tree.IterativePreOrder()
+	fmt.Println()
+
+	fmt.Print("Iterative InOrder (stack): LNR - ")
+	tree.IterativeInOrder()
+	fmt.Println()
+
+	fmt.Print("Iterative BFS (queue): - ")
+	tree.IterativeBFS()
+	fmt.Println()
+
+	fmt.Print("Morris Traversal (without queue or recursion): - ")
+	tree.MorrisTraversal()
+	fmt.Println()
 }
